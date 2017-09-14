a9db7258786456f6968056cdfb760d4b59fa95a8
Ivan Kosolapov <ivan.kosolapov@phystech.edu>
2017-09-14 22:05:48 +0300

    Mark counter fields as readonly for admin.

--- rettiwt/admin.py
+++ rettiwt/admin.py
@@ -6,14 +6,14 @@ from rettiwt.models import Post, Like, Comment
 
 @admin.register(Post)
 class AdminPost(ModelAdmin):
-    pass
-
-
-@admin.register(Like)
-class AdminLike(ModelAdmin):
-    pass
+    readonly_fields = ('comments_count', 'likes_count',)
 
 
 @admin.register(Comment)
 class AdminComment(ModelAdmin):
+    readonly_fields = ('likes_count', )
+
+
+@admin.register(Like)
+class AdminLike(ModelAdmin):
     pass
