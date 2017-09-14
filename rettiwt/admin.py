1a4e326f652733762e4770db81017d4e567458bc
Ivan Kosolapov <ivan.kosolapov@phystech.edu>
2017-09-14 22:00:49 +0300

    Add missing comment and like admins.

--- rettiwt/admin.py
+++ rettiwt/admin.py
@@ -1,9 +1,19 @@
 from django.contrib import admin
 from django.contrib.admin import ModelAdmin
 
-from rettiwt.models import Post
+from rettiwt.models import Post, Like, Comment
 
 
 @admin.register(Post)
 class AdminPost(ModelAdmin):
+    pass
+
+
+@admin.register(Like)
+class AdminLike(ModelAdmin):
+    pass
+
+
+@admin.register(Comment)
+class AdminComment(ModelAdmin):
     pass
